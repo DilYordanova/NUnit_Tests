2f1265439441579f2cdb16226b6ab8d4c9540946
DilYordanova <105145475+DilYordanova@users.noreply.github.com>
2022-05-10 03:41:51 +0300

    Add files via upload

---
+++ NUnit_Summator/Summator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Summator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Summator summator = new Summator();
+
+            double sum = summator.Sum(new double[] { 1, 2, 3, 4, 5, 6 });
+
+            if (sum == 21)
+            {
+                Console.WriteLine("Test Pass");
+            }
+
+            else
+            {
+                Console.WriteLine("Test Fail");
+            }
+
+            double average = summator.Average(new long[] { 5, 6, 7 });
+
+            if (average == 6)
+            {
+                Console.WriteLine("Test Pass");
+            }
+
+            else
+            {
+                Console.WriteLine("Test Fail");
+            }
+
+        }
+    }
+}
